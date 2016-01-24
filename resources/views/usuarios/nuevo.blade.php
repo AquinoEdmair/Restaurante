2a378a084bdfdd2bdef12ef4412311405fa3a3a7
CristhianSZT <cetrehh@gmail.com>
2016-01-24 00:33:26 -0600

    Campos password

--- resources/views/usuarios/nuevo.blade.php
+++ resources/views/usuarios/nuevo.blade.php
@@ -41,14 +41,14 @@
                                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nombre">Contraseña <span class="required">*</span>
                                             </label>
                                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                                <input type="text" name="password" required="required" class="form-control col-md-7 col-xs-12">
+                                                <input type="password" name="password" required="required" class="form-control col-md-7 col-xs-12">
                                             </div>
                                         </div>
                                         <div class="form-group">
                                             <label class="control-label col-md-3 col-sm-3 col-xs-12" for="nombre">Repetir Contraseña <span class="required">*</span>
                                             </label>
                                             <div class="col-md-6 col-sm-6 col-xs-12">
-                                                <input type="text" name="password" required="required" class="form-control col-md-7 col-xs-12">
+                                                <input type="password" name="password" required="required" class="form-control col-md-7 col-xs-12">
                                             </div>
                                         </div>
 
