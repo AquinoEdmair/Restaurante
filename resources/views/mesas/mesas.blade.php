f9144803a6fd0dac15a3cc3c4feca781e9d98faa
Edmair Antonio Aquino <aquinoedmair@outlook.com>
2016-01-26 22:19:16 -0600

    Cambio Mesas

--- resources/views/mesas/mesas.blade.php
+++ resources/views/mesas/mesas.blade.php
@@ -30,7 +30,7 @@
                                                 <tr class="even pointer"> 
                                                     <td class=" ">{{$mesa->nombre}}</td>
                                                     <td class=" ">{{$mesa->estatusmesas->descripcion}}</td>
-                                                    <td class=" "> @if($mesa->asignacion==1) La Mesa esta asignada a una tablet @else La mesa no esta asignada a una tablet @endif</td>
+                                                    <td class=" "> @if($mesa->asignacion==1) La mesa esta asignada a una tablet @else La mesa no esta asignada a una tablet @endif</td>
                                                     <td class=" last">
                                                     {{ Form::open(array('url' => '/mesas/' . $mesa->id)) }} 
                                                         {{ Form::hidden('_method', 'DELETE') }}
