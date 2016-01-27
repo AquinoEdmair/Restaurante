e274306f1c2401b88a81c91f3f8920d23560c775
Edmair Antonio Aquino <aquinoedmair@outlook.com>
2016-01-27 01:40:04 -0600

    Mesa - Pedido - DetallePedido

--- app/Pedido.php
+++ app/Pedido.php
@@ -7,4 +7,8 @@ use Illuminate\Database\Eloquent\Model;
 class Pedido extends Model
 {
     protected $table = 'tbl_pedidos';
+
+    public function detallespedidos(){
+        return $this->hasMany('App\DetallePedido','pedidos_id','id');
+    }
 }
